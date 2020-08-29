06a804d6a0bc7125ea522250db26a9c190b15a79
ARNisUsername <67711082+ARNisUsername@users.noreply.github.com>
2020-08-29 12:55:29 -0700

    Update redditPieChart.py

--- redditPieChart.py
+++ redditPieChart.py
@@ -1,6 +1,7 @@
 import praw
 import matplotlib.pyplot as plt
 
+#Create reddit bot, needed for anaylzing top posts. I used one of my old bots for this
 reddit = praw.Reddit(client_id='zvcDckQJ3VJmLw',
                      client_secret='yUtcmzs7X9IYls7Ru6hBtCFsI8g',
                      password='GR101_ARN',
@@ -13,9 +14,11 @@ subreddit = reddit.subreddit('PoliticalCompassMemes')
 storedDict = {}
 
 for submission in subreddit.top(limit=1000):
+    #For the top 1000 posts, get the flair of the poster
     flairText = submission.author_flair_text
 
     if flairText != None:
+        #If the poster has a flair, store it in a dictionary with the number of times flair has been used
         flairTemp = flairText.split(' - ')
         authorFlair = flairTemp[1]
         if '2' in flairTemp[0]:
@@ -26,11 +29,12 @@ for submission in subreddit.top(limit=1000):
             storedDict[authorFlair] += 1
 
 
-
+#Use the dictionary to get the labels and sizes needed for displaying the matplotlib chart
 labels = list(storedDict.keys())
 sizes = list(storedDict.values())
 colors = ['royalblue', 'gray', 'green', 'yellow', 'orange', 'lime', 'lightsteelblue', 'red', 'mediumorchid', 'lightsalmon']
 
+#Create the pie chart with matplotlib
 plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
 plt.axis('equal')
 plt.show()
